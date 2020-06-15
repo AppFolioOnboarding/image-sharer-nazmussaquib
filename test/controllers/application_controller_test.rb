deb22b738a61fe04be74c939e6b0fc7a8c7b4a0c
Nazmus Saquib <nazmus@ucsb.edu>
2020-06-15 16:53:43 -0700

    ns - setup root path

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get root_path
+    assert_response :success
+    assert_select '#hello', 'Hello World!'
+  end
+end
