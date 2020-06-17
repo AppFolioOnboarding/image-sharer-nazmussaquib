31546f4bd2c8c5b305aae17da91351c7ba6f5654
Nazmus Saquib <nazmus@ucsb.edu>
2020-06-17 09:51:15 -0700

    ns - add image index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def home; end
+  def home
+    @images = Image.order(created_at: :desc)
+  end
 end
