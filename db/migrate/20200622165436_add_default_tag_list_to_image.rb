af021500ecdd5b207c01c038fa3dfe28225f518d
Nazmus Saquib <nazmus@ucsb.edu>
2020-06-23 11:23:52 -0700

    ns - update migration to use SQL

--- db/migrate/20200622165436_add_default_tag_list_to_image.rb
+++ db/migrate/20200622165436_add_default_tag_list_to_image.rb
@@ -1,22 +1,45 @@
 class AddDefaultTagListToImage < ActiveRecord::Migration[5.2]
   def up
-    images = Image.all
-    images.each do |image|
-      # image.tag_list.add('default') if image.tag_list.empty?
-      if image.tag_list.empty?
-        image.tag_list.add('default')
-        image.save
-      end
+    execute <<-SQL
+      INSERT INTO tags (name) VALUES ('default');
+    SQL
+    blank_img_ids.each do |img_id|
+      execute <<-SQL
+        INSERT INTO taggings (tag_id, taggable_id, taggable_type, context)
+        VALUES (#{default_tag_id['id']}, #{img_id['id']}, 'Image', 'tags');
+      SQL
     end
+    execute <<-SQL
+      UPDATE tags SET taggings_count = taggings_count + #{img_ids.length}
+      WHERE id = #{default_tag_id['id']};
+    SQL
   end
 
   def down
-    images = Image.all
-    images.each do |image|
-      if image.tag_list.include?('default')
-        image.tag_list.remove('default')
-        image.save
-      end
-    end
+    execute <<-SQL
+      DELETE
+      FROM taggings
+      WHERE tag_id = #{default_tag_id['id']};
+    SQL
+    execute <<-SQL
+      DELETE
+      FROM tags
+      WHERE id = #{default_tag_id['id']};
+    SQL
+  end
+
+  private
+
+  def default_tag_id
+    @default_tag_id ||= select_one <<-SQL
+      SELECT id FROM tags WHERE name = 'default';
+    SQL
+  end
+
+  def blank_img_ids
+    @blank_img_ids ||= select_all <<-SQL
+      SELECT id FROM images WHERE id NOT IN
+      (SELECT taggable_id FROM taggings WHERE taggable_type = 'Image');
+    SQL
   end
 end
