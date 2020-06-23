8e8e96b5267a05c004d77e83ce18985ef7d1a352
Nazmus Saquib <nazmus@ucsb.edu>
2020-06-22 17:57:00 -0700

    ns - update migration to add default values

--- db/migrate/20200622165436_add_default_tag_list_to_image.rb
+++ db/migrate/20200622165436_add_default_tag_list_to_image.rb
@@ -1,5 +1,22 @@
 class AddDefaultTagListToImage < ActiveRecord::Migration[5.2]
-  def change
-    # change_column_default(:images, :tag_list, from: nil, to: 'default')
+  def up
+    images = Image.all
+    images.each do |image|
+      # image.tag_list.add('default') if image.tag_list.empty?
+      if image.tag_list.empty?
+        image.tag_list.add('default')
+        image.save
+      end
+    end
+  end
+
+  def down
+    images = Image.all
+    images.each do |image|
+      if image.tag_list.include?('default')
+        image.tag_list.remove('default') 
+        image.save
+      end
+    end
   end
 end
