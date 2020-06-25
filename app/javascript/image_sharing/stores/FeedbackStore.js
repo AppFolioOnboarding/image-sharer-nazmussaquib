040bbe92f739e1ccb17c49a6c0747ec94fce8890
Nazmus Saquib <nazmus@ucsb.edu>
2020-06-25 11:33:26 -0700

    ns - add feedback form

--- app/javascript/image_sharing/stores/FeedbackStore.js
+++ app/javascript/image_sharing/stores/FeedbackStore.js
@@ -1,5 +1,22 @@
+import { observable } from 'mobx';
+
 export class FeedbackStore {
   /* Implement your feedback store*/
+  @observable name;
+  @observable feedback;
+
+  constructor() {
+    this.name = '';
+    this.feedback = '';
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  setFeedback(feedback) {
+    this.feedback = feedback;
+  }
 }
 
 export default FeedbackStore;
