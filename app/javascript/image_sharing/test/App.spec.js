edb8e952427e0cd4ba5b3876acad445893905fc2
Nazmus Saquib <nazmus@ucsb.edu>
2020-06-25 11:33:26 -0700

    ns - add feedback form

--- app/javascript/image_sharing/test/App.spec.js
+++ app/javascript/image_sharing/test/App.spec.js
@@ -6,10 +6,12 @@ import { expect } from 'chai';
 import App from '../components/App';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { FeedbackStore } from '../stores/FeedbackStore';
 
 describe('<App />', () => {
   it('mounts and renders header and footer', () => {
-    const wrapper = mount(<App />);
+    const stores = { feedbackStore: new FeedbackStore() };
+    const wrapper = mount(<App stores={stores} />);
     const header = wrapper.find(Header);
     const footer = wrapper.find(Footer);
     expect(header.props().title).to.eq('Tell us what you think');
